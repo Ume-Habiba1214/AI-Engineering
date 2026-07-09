3c4378867cb6da4f34d472232cb0e79f163dcd65
Ume <umehabiba555@gmail.com>
2026-07-09 17:21:52 +0200

    Day 04 Basic

---
+++ Week1/Day04_Basic.py
@@ -0,0 +1,28 @@
+# Star printing function
+
+print("Exercise 01")
+def print_star():
+    print("**********")
+
+print_star()
+print_star()
+print_star()
+print()
+
+
+
+#Function that print your name
+print("Exercise 02")
+def print_name(name):
+    print(f"{name}")
+
+print_name("ali")
+print()
+
+#print square
+print("Exercise 03")
+def print_square(number):
+    print(number*number)
+
+print_square(5)
+print_square(4)
