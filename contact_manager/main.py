320fb77e8ddc639c261e8d1980b93ae050062acd
Ume <umehabiba555@gmail.com>
2026-08-05 14:16:03 +0200

    modify inputs

--- contact_manager/main.py
+++ contact_manager/main.py
@@ -1,9 +1,10 @@
-class ContactMAnager:
+class ContactManager:
     def __init__(self):
         self.contacts=[]
+        self.contact={}
         
 
-    def add_contact(self, name, age, email, city):
+    def add_contact(name, age, email, city):
         self.contact={
 
             "name":name,
@@ -14,8 +15,20 @@ class ContactMAnager:
         self.contacts.append(self.contact)
         print("Contact added successfully!")
 
-    def run(self):
-        name=input("Enter the Name: ")
-        age=int(input("Enter the Age: "))
-        email=input("Enter the Email Address: ")
-        city=input("Enter the city Name: ")
+    def run():
+        print("===== Contact Manager =====")
+        print("1. Add Contact")
+        print("2. Delete Contact")
+        print("3.")
+        print("Exit")
+        print("\n")
+        print("Choose Option: ")
+        option=int(input("Enter the Option number:"))
+        if option==1:
+            name=input("Enter the Name: ")
+            age=int(input("Enter the Age: "))
+            email=input("Enter the Email Address: ")
+            city=input("Enter the city Name: ")
+            ContactManager.add_contact(name,age,email,city)
+
+ContactManager.run()
